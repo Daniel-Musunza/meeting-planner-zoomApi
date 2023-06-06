da987edb22000a2b38733783c428e420fa161db1
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-06-06 16:50:18 +0300

    addreminder api

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ const env = process.env.NODE_ENV || "production";
 
 //insert your API Key & Secret for each environment, keep this file local and never push it to a public repo for security purposes.
 const config = {
-  
+  development: {
+    APIKey: "buxxxxxxxxxxx",
+    APISecret: "4GsyO7xxxxxxx",
+  },
+  production: {
+    APIKey: "rv5PiX4rTxxxxxxg",
+    APISecret: "F113mKKfzQv5ZD4zgxxxxxxxl9Xpy",
+  },
 };
 
 module.exports = config[env];
--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 const express = require("express");
-const cors = require("cors"); //Cross-Origin Resource Sharing
-
+const cors = require("cors");
 const bodyParser = require("body-parser");
-
-//include required modules
 const jwt = require("jsonwebtoken");
 const config = require("./config");
 const rp = require("request-promise");
-
 const app = express();
-const port = 3444;
+const path = require('path');
+const fs = require('fs');
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    limit: "50mb",
-    extended: true,
-  })
-);
+app.use(bodyParser.urlencoded({
+  limit: "50mb",
+  extended: true,
+}));
+app.use(express.static(path.join(__dirname)));
+
+const port = 3444;
+
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express and Zoom API" });
 });
-//zoom
 
-var email, userid, resp;
+// Zoom API
+let email, userid, resp;
 
-//Use the ApiKey and APISecret from config.js
 const payload = {
   iss: config.APIKey,
   exp: new Date().getTime() + 5000,
@@ -34,16 +32,17 @@ const payload = {
 
 const token = jwt.sign(payload, config.APISecret);
 app.post("/meeting", (req, res) => {
-  email = req.body.email;
-  var options = {
+  // email = req.body.email;
+  email ="musunzafestus2019@gmail.com"
+  const options = {
     method: "POST",
-    uri: "https://api.zoom.us/v2/users/" + email + "/meetings",
+    uri: `https://api.zoom.us/v2/users/${email}/meetings`,
     body: {
       topic: "Meeting",
       type: 1,
       settings: {
-        host_video: "true",
-        participant_video: "true",
+        host_video: true,
+        participant_video: true,
       },
     },
     auth: {
@@ -53,26 +52,64 @@ app.post("/meeting", (req, res) => {
       "User-Agent": "Zoom-api-Jwt-Request",
       "content-type": "application/json",
     },
-    json: true, //Parse the JSON string in the response
+    json: true,
   };
 
   rp(options)
     .then(function (response) {
-      console.log("response is: ", response.join_url);
-      // response.status(200).json(response);
-      let dataRes = {
+      console.log("Response is:", response.join_url);
+      const dataRes = {
         join_url: response.join_url,
       };
       res.status(200).json(dataRes);
-
-      // res.send("create meeting result: " + JSON.stringify(response));
     })
     .catch(function (err) {
-      // API call failed...
-      console.log("API call failed, reason ", err);
+      console.log("API call failed, reason", err);
+      res.status(500).json({ error: 'Failed to create meeting' });
     });
 });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+app.get('/data.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data.json'));
+});
+
+app.post('/addreminder', (req, res) => {
+  console.log('Received addreminder request');
+  const newData = req.body;
+
+  const dataPath = path.join(__dirname, 'data.json');
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Failed to read data file' });
+      return;
+    }
+
+    let jsonData;
+    try {
+      jsonData = JSON.parse(data);
+    } catch (parseErr) {
+      console.error(parseErr);
+      res.status(500).json({ error: 'Failed to parse data file' });
+      return;
+    }
+
+    jsonData.push(newData);
+
+    const updatedData = JSON.stringify(jsonData);
+    fs.writeFile(dataPath, updatedData, 'utf8', (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr);
+        res.status(500).json({ error: 'Failed to write data file' });
+        return;
+      }
+
+      res.json({ message: 'Data added successfully', data: jsonData });
+    });
+  });
+});
+
+const hostname = 'localhost';
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
